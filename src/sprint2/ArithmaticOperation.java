56739211e46e650b410cb2abaa933bc4038cd0d5
Abhi <abhishekhinge2000@gmail.com>
2025-08-02 23:44:12 +0530

    javaSprint2

---
+++ src/sprint2/ArithmaticOperation.java
@@ -0,0 +1,35 @@
+package sprint2;
+
+import java.lang.invoke.SwitchPoint;
+import  java.util.Scanner;
+
+public class ArithmaticOperation {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num1 = 10;
+        int num2 = 5;
+        double result = 0;
+
+        System.out.println("Enter + for addition, - for substraction, * for multiplication, / for division");
+        char operator = scanner.next().charAt(0);
+        switch(operator) {
+
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+        System.out.println("Result: " +result);
+    }
+
+}
